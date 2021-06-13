6790f5dd7a4a8b80e8f11f35adef0cd562d63119
Andrew33haa <andreyotroschenko@gmail.com>
2021-06-13 16:06:58 +0200

    fix database tables creation

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .database import Base
+from .department import Department
+from .object import Object
+from .file import File
--- models/database.py
+++ models/database.py
@@ -5,12 +5,9 @@ from sqlalchemy.orm import sessionmaker
 
 sqlalchemy_database_url = f"postgresql://postgres:123@localhost:5432/vm_test"
 
-engine = create_engine(sqlalchemy_database_url, echo=True)
+engine = create_engine(sqlalchemy_database_url, echo=False)
 
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
-
-def create_db():
-    Base.metadata.create_all(engine)
